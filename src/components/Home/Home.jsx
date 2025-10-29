e59e21dcf918ed1ae60f67d9e8ddf36c552308cf
Jethanand <134337163+Jethanandj52@users.noreply.github.com>
2025-10-29 23:56:36 +0500

    first commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import Nav from '../Nav'
+ 
 import Hero from './Hero'
-import ShowProduct from './showProduct'
-import Footer from './Footer'
+ 
 import About from './About'
 import Contact from './Contact'
  
